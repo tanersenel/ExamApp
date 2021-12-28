e189d6c7cbbb56e512e814edf4ea49296f884d1f
Taner ŞENEL <tanercat@hotmail.com>
2021-12-28 21:47:20 +0300

    Add Content Repo

--- ExamApp/Repositories/ContentRepository.cs
+++ ExamApp/Repositories/ContentRepository.cs
@@ -1,5 +1,7 @@
 ﻿using ExamApp.Data;
 using ExamApp.Entities;
+using ExamApp.Extensions;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExamApp.Repositories.Interfaces
@@ -7,19 +9,30 @@ namespace ExamApp.Repositories.Interfaces
     public class ContentRepository : IContentRepository
     {
         private readonly ExamContext _examContext;
+        public ContentRepository(ExamContext examContext)
+        {
+            _examContext = examContext; 
+
+        }
         public Content CreateContent(Content content)
         {
-            throw new System.NotImplementedException();
+            _examContext.Content.Add(content);
+            return content; 
         }
 
         public bool DeleteAllContent()
         {
-            throw new System.NotImplementedException();
+            _examContext.Content.Clear();
+            return true;
         }
 
         public bool DeleteContent(string id)
         {
-            throw new System.NotImplementedException();
+            var content = _examContext.Content.FirstOrDefault(x => x.id == id);
+            if (content != null)  _examContext.Content.Remove(content);
+            return true;
+            
+            
         }
     }
 }
